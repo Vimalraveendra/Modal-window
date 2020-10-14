2230880ae43a5263d365db57a0746dad1a802f32
vimal Raveendran <vimalraveendra@gmail.com>
2020-10-14 06:24:18 +0200

    Updated Dialog & App components

--- src/Components/Dialog/Dialog.js
+++ src/Components/Dialog/Dialog.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Dialog.css'
 
-const Dialog = ({title,subTitle,isOpen})=>{
-    
+const Dialog = ({title,subTitle,isOpen,openWindow})=>{
+  
     return isOpen?(
     <div className='modal'>
  <div className="modal-content" action="/action_page.php">
@@ -11,8 +11,8 @@ const Dialog = ({title,subTitle,isOpen})=>{
       <p>{subTitle}</p>
     
       <div className="clearFix">
-        <button type="button"  className="cancelBtn">Cancel</button>
-        <button type="button"  className="deleteBtn">Delete</button>
+        <button type="button" onClick={()=>openWindow()} className="cancelBtn">Cancel</button>
+        <button type="button"  onClick={()=>openWindow()} className="deleteBtn">Delete</button>
       </div>
     </div>
   </div>
